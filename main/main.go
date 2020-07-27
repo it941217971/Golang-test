bb8cf8a90bea677ef266ca8d899471730f332895
boyuan.zhao <boyuan1.zhao@dhc.com.cn>
2020-07-27 13:48:12 +0800

    Fibonacci series practice code

--- main/main.go
+++ main/main.go
@@ -1,14 +1,37 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Invoker interface {
+	Call(interface{})
+}
+
+type Struct struct{}
+
+func (s *Struct) Call(p interface{}) {
+	fmt.Println("From struct info", p)
+}
+
+type FuncCaller func(interface{})
+
+func (f FuncCaller) Call(p interface{}) {
+	// 调用f函数本体
+	f(p)
+}
 
 func main() {
-
-	var a []int
-	a = []int{1, 2, 3}
-	a = append(a, ) // 删除开头1个元素
-	fmt.Println(a)
-
+	// 声明接口变量
+	var invoker Invoker
+	// 实例化结构体
+	s := new(Struct)
+	// 将实例化的结构体赋值到接口
+	invoker = s
+	// 使用接口调用实例化结构体的方法Struct.Call
+	invoker.Call("hello")
+	// 将匿名函数转为FuncCaller类型，再赋值给接口
+	invoker = FuncCaller(func(v interface{}) {
+		fmt.Println("from function", v)
+	})
+	// 使用接口调用FuncCaller.Call，内部会调用函数本体
+	invoker.Call("hello")
 }
